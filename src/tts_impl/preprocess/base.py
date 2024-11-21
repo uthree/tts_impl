1902b383316d2ca0a63b6e65d6e436b92c6e5477
uthree <uthree271828@gmail.com>
2024-11-21 20:17:09 +0900

    add docstring

--- src/tts_impl/preprocess/base.py
+++ src/tts_impl/preprocess/base.py
@@ -15,16 +15,37 @@ class DataCollector:
     def __iter__(self) -> Generator[dict, None, None]:
         pass
 
+    def prepare(self):
+        """
+        This method is called only once before preprocessing is performed.
+        Implement this if preparation processing is required.
+        """
+        pass
+
 
 class Extractor:
     """
     Base class for extracting features from raw data.
     """
 
-    def extract(data: dict) -> dict:
+    def extract(data: dict[str, Any]) -> dict[str, Any]:
+        """
+        Processing such as feature extraction.
+        """
+        return data
+
+    def prepare(self):
+        """
+        This method is called only once before preprocessing is performed.
+        Implement this if preparation processing is required.
+        """
         pass
 
     def __call__(self, *args, **kwargs):
+        """
+        Alias.
+        Same as the method `extract`.
+        """
         return self.extract(*args, **kwargs)
 
 
@@ -46,7 +67,7 @@ class FunctionalExtractor(Extractor):
         self.fn = fn
         self.nograd = nograd
 
-    def extract(self, data: dict) -> dict:
+    def extract(self, data: dict[str, Any]) -> dict[str, Any]:
         target_data = data[self.input_key]
         if self.nograd:
             with torch.no_grad():
@@ -65,15 +86,30 @@ class CacheWriter:
     def __init__(
         self,
         cache_dir: Union[str, os.PathLike] = "dataset_cache",
-        remove_old_cache=False,
+        remove_old_cache=True,
         *args,
         **kwargs,
     ):
-        if remove_old_cache:
-            if self.cache_dir.exists():
-                shutil.rmtree(cache_dir)
+        """
+        Args
+            cache_dir: Union[str, os.PathLike], Directory for caching datasets.
+            remove_old_cache: bool
+        """
+        self.remove_old_cache = remove_old_cache
+        self.cache_dir = cache_dir
 
-        self.cache_dir = Path(cache_dir)
+    def prepare(self):
+        """
+        This method is called only once before preprocessing is performed.
+        Implement this if preparation processing is required.
+
+        The default implementation is to delete old caches and create a directory for the cache.
+        """
+        if self.remove_old_cache:
+            if self.cache_dir.exists():
+                shutil.rmtree(self.cache_dir)
+
+        self.cache_dir = Path(self.cache_dir)
         if not self.cache_dir.exists():
             self.cache_dir.mkdir()
         self.counter = 0
@@ -95,7 +131,7 @@ class CacheWriter:
 
 class Preprocessor:
     """
-    Base class of preprocessor
+    A class that performs preprocessing collectively.
     """
 
     def __init__(
@@ -140,6 +176,13 @@ class Preprocessor:
         Execute preprocessing.
         """
         assert self.writer is not None, "CacheWriter required, but not given."
+
+        tqdm.write("Preparing submodules...")
+        for c in self.collectors:
+            c.prepare()
+        for e in self.extractors:
+            e.prepare()
+        self.writer.prepare()
 
         tqdm.write("Start preprocessing...")
         for collector in self.collectors:
