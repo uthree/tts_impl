cf2e0fb600d4502953eebd05256b81e59aa844e1
uthree <uthree271828@gmail.com>
2025-05-01 01:20:07 +0900

    fix typo

--- test/test_mas.py
+++ test/test_mas.py
@@ -6,7 +6,7 @@ from tts_impl.functional.monotonic_align import maximum_path
 @pytest.mark.parametrize("batch_size", [1, 2])
 @pytest.mark.parametrize("text_length", [100, 200])
 @pytest.mark.parametrize("feat_length", [1000, 2000])
-@pytest.mark.parametrize("algorithm", ["mumba", "cython", "jit1", "jit2", "triton"])
+@pytest.mark.parametrize("algorithm", ["numba", "cython", "jit1", "jit2", "triton"])
 def test_mas(batch_size: int, text_length: int, feat_length: int, algorithm: str):
     attn = torch.randn(batch_size, text_length, feat_length)
     attn_mask = torch.ones_like(attn)
