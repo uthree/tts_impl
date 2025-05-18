9237e2d82c76e9cef95b92eff83d113d5b40d25c
uthree <uthree271828@gmail.com>
2025-05-18 21:10:01 +0900

    add EMA-based instance norm

--- test/test_mingru.py
+++ test/test_mingru.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
 from tts_impl.net.common.mingru import MinGRU
+from tts_impl.net.base.state import sanity_check_stateful_module
 
 
 @pytest.mark.parametrize("d_model", [64, 128])
@@ -24,11 +25,4 @@ def test_mingru_sanity_check(d_model, d_hidden, batch_size, seq_len):
     model = MinGRU(d_model=d_model, d_hidden=d_hidden)
     x = torch.randn(batch_size, seq_len, d_model)
     y_par, _ = model(x)
-    y_seq = []
-    h_t = None
-    for x_t in x.unbind(dim=1):
-        x_t = x_t[:, None]
-        y_t, h_t = model(x_t, h_t)
-        y_seq.append(y_t)
-    y_seq = torch.cat(y_seq, dim=1)
-    assert torch.allclose(y_par, y_seq, atol=1e-4)
+    sanity_check_stateful_module(model, x)
