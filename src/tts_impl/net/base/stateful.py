add stateful sequential module, fix normalization layer

src/tts_impl/net/base/stateful.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Sequence
 
 import torch
 import torch.nn as nn
@@ -89,6 +89,41 @@ class PointwiseModule:
     """
 
     pass
+
+
+class StatefulModuleSequential(StatefulModule):
+    def __init__(self, *stateful_modules: Sequence[StatefulModule]):
+        super().__init__()
+        self.stateful_modules = nn.ModuleList(stateful_modules)
+        self.h_dim_list = []
+
+    def _initial_state(self, x: torch.Tensor) -> torch.Tensor:
+        hs = []
+        h_dim_list = []
+        for module in self.stateful_modules:
+            h = module._initial_state(x)
+            hs.append(h)
+            h_dim_list.append(h.shape[2])
+        self.h_dim_list = h_dim_list
+        return torch.cat(hs, dim=2)
+
+    def _parallel_forward(
+        self, x: torch.Tensor, h: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+
+        # calculate sum of all hidden state dimension if it's not already calculated.
+        if self.h_dim_list is None:
+            self._initial_state(x)
+
+        # forward pass each layer
+        hs = list(torch.split(h, self.h_dim_list, dim=2))
+        for i in range(len(self.stateful_modules)):
+            x, hs[i] = self.stateful_modules[i](x, hs[i])
+        hs = torch.cat(hs, dim=2)
+        return x, hs
+
+    def append(self, m):
+        self.stateful_modules.append(m)
 
 
 def sanity_check_stateful_module(

src/tts_impl/net/common/mingru.py
@@ -47,7 +47,6 @@ class MinGRU(StatefulModule):
         self,
         d_model: int,
         d_hidden: Optional[int] = None,
-        p_dropout: float = 0.0,
         bias: bool = True,
     ):
         """
@@ -56,7 +55,6 @@ class MinGRU(StatefulModule):
         Args:
             d_model: int, input dimension
             d_hidden: Optional[int], hidden dimension, if `None` given, set same to `d_model` automatically.
-            p_dropout: float, probabilities of dropout
             bias: bool, if True is given, add bias to Linear module., default is `True`
         """
         super().__init__()
@@ -64,12 +62,11 @@ class MinGRU(StatefulModule):
         if d_hidden is None:
             d_hidden = d_model
         self.d_hidden = d_hidden
-        self.p_dropout = p_dropout
         self.linear_h = nn.Linear(d_model, d_hidden, bias=bias)
         self.linear_z = nn.Linear(d_model, d_hidden, bias=bias)
 
     def _sequential_forward(
-        self, x: torch.Tensor, h_prev: Optional[torch.Tensor]
+        self, x: torch.Tensor, h_prev: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         z = torch.sigmoid(self.linear_z(x))
         h_tilde = g(self.linear_h(x))
@@ -77,7 +74,7 @@ class MinGRU(StatefulModule):
         return h, h
 
     def _parallel_forward(
-        self, x: torch.Tensor, h_0: Optional[torch.Tensor]
+        self, x: torch.Tensor, h_0: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         k = self.linear_z(x)
         log_z = -F.softplus(-k)

src/tts_impl/net/common/normalization.py
@@ -211,7 +211,7 @@ class ExponentialMovingAverage(StatefulModule):
     def __init__(
         self,
         d_model: int,
-        alpha: float = 0.99,
+        alpha: float = 0.995,
         trainable: bool = True,
         initial_value: float = 0.0,
     ):
@@ -300,12 +300,13 @@ class EmaLayerNorm(StatefulModule):
         self,
         d_model: int,
         elementwise_affine: bool = True,
-        alpha_mu: float = 0.99,
-        alpha_sigma: float = 0.99,
+        alpha_mu: float = 0.995,
+        alpha_sigma: float = 0.998,
         alpha_trainable: bool = True,
         eps: float = 1e-12,
     ):
         super().__init__()
+        self.d_model = d_model
         self.elementwise_affine = elementwise_affine
         if elementwise_affine:
             self.beta = nn.Parameter(torch.zeros(1, 1, d_model))
@@ -324,11 +325,11 @@ class EmaLayerNorm(StatefulModule):
         """
         Args:
             x: shape=(batch_size, seq_len, d_model)
-            h: shape=(batch_size, seq_len, 2)
+            h: shape=(batch_size, 1, 2)
 
         Returns:
             x: shape=(batch_size, seq_len, d_model)
-            h: shape(batch_size, seq_len, 2)
+            h: shape(batch_size, 1, 2)
         """
         # expand state to mean and standard deviation.
         h_mu, h_sigma = torch.chunk(h, 2, dim=2)
@@ -359,8 +360,8 @@ class EmaLayerNorm(StatefulModule):
 
     def _initial_state(self, x):
         batch_size = x.shape[0]
-        h_mu = torch.zeros(batch_size, 1, 1, device=x.device)
-        h_sigma = torch.ones(batch_size, 1, 1, device=x.device)
+        h_mu = torch.mean(x[:, :1], dim=2, keepdim=True)
+        h_sigma = torch.std(x[:, :1], dim=2, keepdim=True)
         h = torch.cat([h_mu, h_sigma], dim=2)
         return h
 
@@ -375,16 +376,17 @@ class EmaInstanceNorm(StatefulModule):
         self,
         d_model: int,
         elementwise_affine: bool = True,
-        alpha_mu: float = 0.99,
-        alpha_sigma: float = 0.99,
+        alpha_mu: float = 0.995,
+        alpha_sigma: float = 0.998,
         alpha_trainable: bool = True,
         eps: float = 1e-12,
     ):
         super().__init__()
+        self.d_model = d_model
         self.elementwise_affine = elementwise_affine
         if elementwise_affine:
             self.beta = nn.Parameter(torch.zeros(1, 1, d_model))
-            self.gamma = nn.Parameter(torch.ones(1, 1, d_model))
+            self.gamma = nn.Parameter(torch.ones(1, 1, 1))
         self.ema_mu = ExponentialMovingAverage(
             d_model=d_model,
             alpha=alpha_mu,
@@ -405,11 +407,11 @@ class EmaInstanceNorm(StatefulModule):
         """
         Args:
             x: shape=(batch_size, seq_len, d_model)
-            h: shape=(batch_size, seq_len, 2)
+            h: shape=(batch_size, 1, d_model * 2)
 
         Returns:
             x: shape=(batch_size, seq_len, d_model)
-            h: shape(batch_size, seq_len, 2)
+            h: shape(batch_size, 1, d_model * 2)
         """
         # expand state to mean and standard deviation.
         h_mu, h_sigma = torch.chunk(h, 2, dim=2)
@@ -440,7 +442,7 @@ class EmaInstanceNorm(StatefulModule):
 
     def _initial_state(self, x):
         batch_size = x.shape[0]
-        h_mu = torch.zeros(batch_size, 1, 1, device=x.device)
-        h_sigma = torch.ones(batch_size, 1, 1, device=x.device)
+        h_mu = x[:, :1]
+        h_sigma = torch.ones(batch_size, 1, self.d_model, device=x.device)
         h = torch.cat([h_mu, h_sigma], dim=2)
         return h