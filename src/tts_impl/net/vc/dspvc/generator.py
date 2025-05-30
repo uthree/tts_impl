62dae5f1d94346c3ebaffeef4035d7fc2b8be065
uthree <uthree271828@gmail.com>
2025-05-31 02:16:19 +0900

    add implementation of f0 loss and uv loss

--- src/tts_impl/net/vc/dspvc/generator.py
+++ src/tts_impl/net/vc/dspvc/generator.py
@@ -1,17 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from tts_impl.functional.midi import freq2note, note2freq
 from tts_impl.net.base.stateful import StatefulModule
 from tts_impl.net.common.grux import Grux
 from tts_impl.utils.config import derive_config
+import math
 
 
 @derive_config
 class Encoder(StatefulModule):
     def __init__(
         self,
-        in_channels: int,
+        in_channels: int = 80,
         d_model: int = 128,
         num_layers: int = 4,
         kernel_size: int = 4,
@@ -21,10 +21,47 @@ class Encoder(StatefulModule):
         num_f0_classes: int = 192,
     ):
         super().__init__()
+        self.num_f0_classes = num_f0_classes
+        self.phoneme_embedding_dim = phoneme_embedding_dim
+        self.fmin = fmin
+        self.fmax = fmax
+
         self.pre = nn.Linear(in_channels, d_model)
         self.grux = Grux(d_model=d_model, num_layers=num_layers)
         self.to_phone = nn.Linear(d_model, phoneme_embedding_dim)
         self.to_f0 = nn.Linear(d_model, num_f0_classes)
 
-    def forward(self, x):
-        pass
+    def forward(self, x, h=None):
+        x = x.transpose(1, 2)
+        x = self.pre(x)
+        x, h = self.grux(x)
+        phone_emb = self.to_phone(x).transpose(1, 2)
+        f0_logits = self.to_f0 = self.to_f0(x).transpose(1, 2)
+        return phone_emb, f0_logits, h
+
+    def f0_loss(self, f0_logits, f0, uv):
+        f0_logits, uv_logits = torch.split(f0_logits, [self.num_f0_classes - 1, 1])
+        uv_hat = torch.sigmoid(uv_logits)
+        loss_uv = F.binary_cross_entropy(uv_hat, uv)
+        log_fmin = math.log(self.fmin)
+        log_fmax = math.log(self.fmax)
+        delta_log_f0 = log_fmax - log_fmin
+        f0_label = (torch.log(f0) - log_fmin) / delta_log_f0 * (self.num_f0_classes - 1)
+        loss_f0 = torch.cross_entropy(f0_logits, f0_label)
+        return loss_f0, loss_uv
+
+    def decode_f0(self, f0_logits, k: int = 2):
+        f0_logits, uv = torch.split(f0_logits, [self.num_f0_classes - 1, 1])
+        uv = (torch.sigmoid(uv) > 0.5).float()
+        log_fmin = math.log(self.fmin)
+        log_fmax = math.log(self.fmax)
+        delta_log_f0 = log_fmax - log_fmin
+        topk_result = torch.topk(f0_logits, dim=1, k=k)
+        indices = topk_result.indices.float()
+        values = topk_result.values
+        probs = torch.softmax(values, dim=1)
+        log_f0 = (values * probs).sum(dim=1) / (
+            self.num_f0_classes - 1
+        ) * delta_log_f0 + log_fmin
+        f0 = torch.exp(log_f0) * uv
+        return f0
