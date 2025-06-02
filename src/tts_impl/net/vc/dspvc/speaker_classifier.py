98290c10b0a75ddef7b471545a6f5e7238ccdfe9
uthree <uthree271828@gmail.com>
2025-06-03 00:04:06 +0900

    fix

--- src/tts_impl/net/vc/dspvc/speaker_classifier.py
+++ src/tts_impl/net/vc/dspvc/speaker_classifier.py
@@ -7,9 +7,9 @@ from tts_impl.utils.config import derive_config
 
 @derive_config
 class SpeakerClassifier(nn.Module):
-    def __init__(self, phone_embedding_dim: int = 64, num_speakers: int=256, channels:int= 128):
+    def __init__(self, phone_embedding_dim: int = 64, num_speakers: int=256, channels:int= 128, num_layers: int=4):
         super().__init__()
-        self.convnext = ConvNeXt1d(phone_embedding_dim, channels, channels, ffn_channels=channels*3, grn=True, glu=True)
+        self.convnext = ConvNeXt1d(phone_embedding_dim, channels, channels, ffn_channels=channels*3, grn=True, glu=True, num_layers=num_layers)
         self.post = nn.Conv1d(channels, num_speakers, 1)
 
     def forward(self, x):
